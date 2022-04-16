a6c43aba53a1a697fb4902c9fc96cf3739c90cf0
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-16 16:28:28 +0900

    Ch09. JS 선행 - 01.개요

---
+++ test/js선행/main.js
@@ -0,0 +1,22 @@
+console.log('HEROPY!');
+
+/* 
+ JavaScript 선행 학습
+ 01. 개요
+  1) 표기법
+   -dash-case(kebab-case): HTML, CSS에서 주로 사용
+   -snake_case: HTML, CSS에서 주로 사용
+   -camelCasee: JS에서 주로 사용
+   -ParcelCase: JS에서 특정 상황에서 사용(함수 이름 명시)
+  2) Zero-based Numbering: 숫자를 0부터 센다.
+  3) 주석
+   - //, /* 한 줄 메모 */
+   /*
+   - /*
+    여러줄 메모
+   */
+  
+  /*
+
+  */
+
