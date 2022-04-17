4a99d6976387f54a73a95e643042650dcf7433a3
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-17 22:51:52 +0900

    Ch 9. JS 선행: 02. 데이터 종류

--- test/js선행/main.js
+++ test/js선행/main.js
@@ -14,9 +14,51 @@ console.log('HEROPY!');
    /*
    - /*
     여러줄 메모
-   */
+   */ 
   
   /*
+  02. 데이터 종류(자료형)
+   1) String: 문자데이터로 따옴표를 사용한다.
+    let myName = "HEROPY";
+    let hello = `Hello ${myName}?!` -> 백틱도 사용이 가능하다(보간법?)
+
+    console.log(hello); // Hello HEROPY
+
+    2) Number(숫자 데이터)
+     let number = 123;
+     let opacity = 1.57
+
+    3) Boolean: true, false 두 가지 값 밖에 없는 논리 데이터이다.
+     let checked = true;
+     let isShow = false;
+
+    4) Undefined: 값이 할당되지 않은 상태를 나타낸다.
+      let undef;
+      let obj = { abc: 123};
+
+      console.log(undef); // undefined
+      console.log(obj.abc); // 123
+      console.log(obj.xyz); // undefined
+
+    5) Null: 어떠한 값이 의도적으로 비어있음을 의미한다.
+      let empty = null;
+
+      console.log(empty); // null
+  
+    6) Object(객체 데이터): 여러 데이터를 Key:Value 형태로 저장한다. { }
+      let user = {
+        //Key: Value,
+        name: 'HEROPY',
+        age: 85,
+        isValid: true
+      };
+
+      console.log(user.name); // HEROPY
+
+    7) Array(배열 데이터): 여러 데이터를 순차적으로 저장한다. [ ]
+      let fruits = ['Apple', 'Banana', 'Cherry'];
+      
+      console.log(fruits[0]); // 'Apple'
 
   */
 
